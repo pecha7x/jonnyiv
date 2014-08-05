a1a4ebc94b12a58cdaf80b61f9e810f971e9964f
apecherin <artempecherin@gmail.com>
2014-08-05 11:54:37 +0400

    chage harry

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(:email => 'jonnyspree@gmail.com', :password => '123456', :password_confirmation => '123456')
+AdminUser.create!(:email => 'jonnyspree@gmail.com', :password => '12345678', :password_confirmation => '12345678')
