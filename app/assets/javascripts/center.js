45de1baec30434a691de8d7a779abfd71715a370
apecherin <artempecherin@gmail.com>
2014-08-18 10:14:58 +0400

    temp

--- app/assets/javascripts/center.js
+++ app/assets/javascripts/center.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 
     jQuery.fn.center = function () {
         this.css("position","fixed");
-        console.log("dd")
         this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
             $(window).scrollTop()  + 80) + "px");
         this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
